8b3c4a86e69b0d0f6a3c8e81ce468c1eaaf67a96
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-09-16 23:03:22 +0900

    create and edit raindrop-test.sh

---
+++ raindrop-test.sh
@@ -0,0 +1,13 @@
+# collectionId: -1 は未整理フォルダの識別子
+# ゴミ箱を除く全項目のコレクションの識別子は0
+# $RAINDROP_TEST_TOKEN はユーザーが取得した自分用のraindrop test token, 事前に環境変数として設定しておく
+
+auth="Authorization: Bearer $RAINDROP_TEST_TOKEN"
+root='https://api.raindrop.io/rest/v1'
+
+curl -s "$root/raindrops/-1?page=0&perpage=30" -H $auth | jq -r '.items[].link'
+# 未整理フォルダの最初のページの30項目を取得し、そこからURLを取得
+# pageクエリーは0 index(0から始まる)
+curl -s "$root/user/stats" -H $auth | jq '.items[] | select(._id==-1) | .count'
+# 未整理フォルダの項目数を表示
+# 全項目数を30で割った数が工程の繰り返し回数になる
