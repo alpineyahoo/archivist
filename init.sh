1376d54c220f988444e1c63e3ba02c0bc3714bda
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-09-28 03:43:22 +0900

    edit init.sh

--- init.sh
+++ init.sh
@@ -3,10 +3,7 @@
 export PATH="$PATH:/usr/local/bin:/usr/bin:/bin"
 
 case $(gum choose "default setting" "customize env vars" --header "You prefer...") in
-	"default setting") mkdir -p $HOME/.config/archivist; mkdir -p $HOME/.local/share/archivist; echo "~/.config/archivist\nand\n~/.local/share/archivist\ndirectories were created." ;;
+	"default setting") mkdir -p $HOME/.config/archivist; echo "~/.config/archivist directory was created." ;;
 	"customize env vars") echo "Script did nothing and you'll be ready after setting env vars!!" ;;
 	* ) echo "You just interrupted or unexpected error occurred." ;;
 esac
-
-# mkdir -p $HOME/.config/archivist
-# mkdir -p $HOME/.local/share/archivist
