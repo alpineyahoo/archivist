dfd7047bd57833057277a18391367577a9ec3a6b
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2024-09-28 00:55:04 +0900

    edit raindrop-test.sh

--- raindrop-test.sh
+++ raindrop-test.sh
@@ -8,7 +8,9 @@ root='https://api.raindrop.io/rest/v1'
 curl -s "$root/raindrops/-1?page=0&perpage=30" -H $auth | jq -r '.items[].link'
 # 未整理フォルダの最初のページの30項目を取得し、そこからURLを取得
 # pageクエリーは0 index(0から始まる)
+
 curl -s "$root/user/stats" -H $auth | jq '.items[] | select(._id==-1) | .count'
 # 未整理フォルダの項目数を表示
+
 curl -s "$root/user/stats" -H $auth | jq '.items[] | select(._id==-1) | .count' | awk '{print $1"/30"}' | bc
 # 全項目数を30で割った数が工程の繰り返し回数になる(1...x+1 または 0...x)
